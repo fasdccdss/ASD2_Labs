30c55d71ee116f8051327325fa493d5ee01cf052
Tymur <nsdfd36@gmail.com>
2026-04-29 05:39:30 +0300

    Wrong array implementation

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Net.Http.Headers;
+
+class Program
+{
+    static void Main()
+    {
+        double[] values = Array.Empty<double>();
+        RecursionThree.F(5, 10, values, 0, false);
+    }
+}
--- src/RecursionAlgorithms.cs
+++ src/RecursionAlgorithms.cs
@@ -1,8 +1,5 @@
 using System;
-using System.IO.Compression;
-using System.IO.Pipelines;
-using System.Runtime;
-using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 public class RecursionOne
 {
@@ -40,6 +37,16 @@ public class RecursionTwo()
 {
     private static double F(int n, double x, int i = 1, double result = 1, double sum = 1)
     {
+        // guards
+        if (n <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(n), "Value of i must be positive");
+        }
+        if (Math.Abs(x) >= 1_000_000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(x), "x must satisfy |x| < 10^6");
+        }
+
         if (i == n)
         {
             Console.WriteLine($"F({i}) Sum: {sum}");
@@ -55,23 +62,14 @@ public class RecursionTwo()
     }
     public static double Compute(int n, double x)
     {
-        // guards
-        if (n <= 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(n), "Value of i must be positive");
-        }
-        if (Math.Abs(x) >= 1_000_000)
-        {
-            throw new ArgumentOutOfRangeException(nameof(x), "x must satisfy |x| < 10^6");
-        }
-
         return F(n, x);
     }
 }
 
 public class RecursionThree()
 {
-    private static double F(int n, double x)
+    // in this implementation we would first go down the order, than go up and calculate the sum
+    public static double F(int n, double x, double[] values, double sum, bool initilized = false)
     {
         // guards
         if (n <= 0)
@@ -82,32 +80,29 @@ public class RecursionThree()
         {
             throw new ArgumentOutOfRangeException(nameof(x), "x must satisfy |x| < 10^6");
         }
-
-        if (n == 1) return 1;
         // operation
-        double result = F(n - 1, x) * (x * x) / (4 * (n - 1) * (n - 1) - 2 * (n - 1));
-        Console.WriteLine($"F({n}) Result: {result}");
-        return result;
-    }
-    private static double Sum(int n, double x)
-    {
-        if (n == 1) return F(1, x);
-
-        return F(n, x) + Sum(n - 1, x);
-    }
-    public static double Compute(int n, double x)
-    {
-        if (n <= 0)
+        if (!initilized)
         {
-            throw new ArgumentOutOfRangeException(nameof(n), "Value of n must be positive");
+            values[0] = 1;
+            sum = 0;
+            initilized = true;
         }
-        if (Math.Abs(x) >= 1_000_000)
+        if (n != 1)
         {
-            throw new ArgumentOutOfRangeException(nameof(x), "x must satisfy |x| < 10^6");
-        }
+            values[n] = 
+            F(n - 1, x, values, sum, initilized) * (x * x) / (4 * (n - 1) * (n - 1) - 2 * (n - 1));
 
-        double sum = Sum(n, x);
-        Console.WriteLine($"F({n}) Sum: {sum}");
+            Console.WriteLine($"F({n}); value: {values[n]}");
+            return values[n];
+        }
+        else if (n < values.Length)
+        {
+            int z = n;
+            sum += values[z - 1];
+            Console.WriteLine($"n = {n}; sum = {sum}");
+            return F(n++, x, values, sum, initilized) * (x * x) / (4 * (n - 1) * (n - 1) - 2 * (n - 1));
+        }
+        Console.WriteLine($"n = {n}; sum = {sum}");
         return sum;
     }
 }
