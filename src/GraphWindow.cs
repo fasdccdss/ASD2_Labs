bd8716b1eadecdc76b774f94b38cf5fe72082bc4
Tymur <nsdfd36@gmail.com>
2026-05-04 17:53:28 +0300

    save

--- src/GraphWindow.cs
+++ src/GraphWindow.cs
@@ -33,46 +33,72 @@ public class GraphWindow : Form
     }
     /* DIRECTED GRAPH */
     /// <summary>
-    /// to implement this we can take the N amount of "Peaks", 
-    /// allocate a square of size X for each peak,
-    /// than using this allocated space space, 
-    /// calculate the overall size of the border the graph would be constructed inside
     /// 
-    /// Graph itself is constructed using a grid that is built after the border, 
-    /// each peak is assigned to its respective grid cell
-    /// 
-    /// It is best to draw vertices starting from the middle
     /// </summary>
-    private static void DrawDirectedGraph(int seed, int wWidth, int wHeight,
-     int n, int cellSize = 80)
+    private static void DrawDirectedGraph(int seed,
+     int n, int minSpace = 30, int vertRadius = 5)
     {
-        PointF[] vertices = new PointF[n];
+        int minRows = 3;
+        int minColumns = 3;
+        int minEdges = 2 * (minRows + minColumns) - 4; // 8
 
-        int perimeter = n - 1;
+        int vertices = n - 1;
 
-        // minimum grid size that can hold all perimeter vertices
-        int perSide = (int)MathF.Ceiling(perimeter / 4f);
-        int columns = perSide;
-        int rows = perSide;
+        int maxRows = minRows;
+        int lRows = minRows;
+        int rRows = minRows;
 
-        int requiredWidth = columns * cellSize;
-        int requiredHeight = rows * cellSize;
+        int maxColumns = minColumns;
+        int uColumns = minColumns;
+        int dColumns = minColumns;
 
-        int startX = (wWidth - requiredWidth) / 2;
-        int startY = (wHeight - requiredHeight) / 2;
-
-        PointF Cell(int col, int row) => new PointF(
-            startX + col * cellSize,
-            startY + row * cellSize
-        );
-        // placing vertices
-        for (int x = 0; x < rows; x++)
+        if (vertices > minEdges)
         {
-            for (int y = 0; y < columns; y++)
+            bool increaseRows = false;
+            bool l = true;
+
+            for (int x = 0; x < vertices - minEdges; x++)
             {
-                
+                if (x != 0 && x % 2 == 0)
+                {
+                    increaseRows = !increaseRows;
+                }
+
+                if (increaseRows)
+                {
+                    maxRows++;
+
+                    if (l)
+                    {
+                        lRows++;
+                        l = !l;
+                    }
+                    else
+                    {
+                        rRows++;
+                        l = !l;
+                    }
+                }
+                else
+                {
+                    maxColumns++;
+
+                    if (l)
+                    {
+                        uColumns++;
+                        l = !l;
+                    }
+                    else
+                    {
+                        dColumns++;
+                        l = !l;
+                    }
+                }
             }
         }
+
+        int rectWidth = maxColumns * minSpace;
+        int rectHeight = maxRows * minSpace;
     }
     /* UNDIRECTED GRAPH */
     private static void DrawGraph()
